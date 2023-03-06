50011c7af2025bb86500714df0987f0e12af78fa
Martin Letras <martin.letras@fau.de>
2023-03-06 08:51:10 +0100

    updating cuda implementations of CGA algorithm

---
+++ cGA3D-GPU/source.sh
@@ -0,0 +1,5 @@
+# codesigns57
+
+export PATH="/usr/local/cuda-12.1/bin:${PATH}"
+export LD_LIBRARY_PATH="/usr/local/cuda-12.1/lib:${LD_LIBRARY_PATH}"
+export LD_RUN_PATH="/usr/local/cuda-12.1/lib:${LD_RUN_PATH}"
