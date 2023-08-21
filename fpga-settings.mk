98db87b1bbd862aa27598019ce1e00dab431060f
Martin Letras <martin.letras@fau.de>
2023-08-21 18:08:22 +0200

    updating part to run on vivado 2022

--- fpga-settings.mk
+++ fpga-settings.mk
@@ -1,6 +1,6 @@
 export BOARD=zcu102
-export XILINX_PART=xczu9eg-ffvb1156-2-i-es2
-export XILINX_BOARD=xilinx.com:zcu102:part0:2.0
+export XILINX_PART=xczu9eg-ffvb1156-2-e
+export XILINX_BOARD=xilinx.com:zcu102:part0:3.4
 export FC_CLK_PERIOD_NS=50
 export PER_CLK_PERIOD_NS=100
 export SLOW_CLK_PERIOD_NS=30517
